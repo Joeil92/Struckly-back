58fb07c04488a32d4c65960b41f6e1322dab0c51
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-05-29 12:35:06 +0200

    Add first e2e test to user POST route

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -1,20 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
-import { UsersModule } from '../../src/users/users.module'
+import { AppModule } from '../../src/app.module'
+import { CreateUserDto } from '../../src/users/dto/create-user.dto'
+import * as request from 'supertest'
 
 describe('Users - /users (e2e)', () => {
   let app: INestApplication
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [UsersModule],
+      imports: [AppModule],
     }).compile()
     app = moduleRef.createNestApplication()
     await app.init()
   })
 
-  it('Create [POST] /users', () => {
-    throw new Error('Not implemented')
+  it('Create [POST] /users', async () => {
+    const userDto: CreateUserDto = {
+      email: 'john.doe@gmail.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: '123456789',
+      gender: 'male',
+    }
+
+    return request(app.getHttpServer())
+      .post('/users')
+      .send(userDto)
+      .expect(201)
+      .then(({ body }) => {
+        expect(body).toMatchObject({
+          access_token: expect.any(String),
+          refresh_token: expect.any(String),
+        })
+      })
   })
 
   afterAll(async () => {
